1c4b00bbb09c6a569f2b736fdc44d55c8fd1da4b
vinodhini7 <vinodhini.s3@gmail.com>
2022-03-14 21:24:27 +0530

    committed my code

--- printOnetoTen.java
+++ printOnetoTen.java
@@ -3,13 +3,13 @@ package whileLoops;
 public class printOnetoTen {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		printOnetoTen pt = new printOnetoTen();
 		pt.OnetoTen();
 	}
 
 	private void OnetoTen() {
-		// TODO Auto-generated method stub
+		
 		int i=0;
 		int count =0;
 		while(count<=10) {
